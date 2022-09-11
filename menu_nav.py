7bf157034f755c3f8367069c8d8e68b48ccd2594
clarabs <maria.meier@luizalabs.com>
2022-09-11 19:54:59 -0300

    corrigir

--- menu_nav.py
+++ menu_nav.py
@@ -1,9 +1,12 @@
 import os
 import boas_vindas
-def nav( ):
+
+
+def nav():
 
     os.system("cls")
-    red2 = int(input('Digite o número de uma das opções abaixo: \n 1 - Menu de Cadastro; \n 2 - Menu de Vendas; \n 3 - Voltar.\n'))
+    red2 = int(input(
+        'Digite o número de uma das opções abaixo: \n 1 - Menu de Cadastro; \n 2 - Menu de Vendas; \n 3 - Voltar.\n'))
     if red2 == 1:
         print()
     elif red2 == 2:
