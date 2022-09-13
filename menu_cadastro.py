ddc4a8f595d6d89b871a680d78597221a3480b5d
George Freire <georgelofre@gmail.com>
2022-09-12 22:12:32 -0300

    Mais tratamento de erro

--- menu_cadastro.py
+++ menu_cadastro.py
@@ -20,10 +20,10 @@ def MenuCadastro():
                 preco = float(input('Qual o preço: R$ '))
                 produto.append(nome)
                 produto.append(preco)
-                print(f'Nome do produto: {nome} ===> Preço: R$ {preco:.2f}')
-                print('Parabéns, seu produto foi cadastrado com sucesso!')
+                print(f'Nome do produto: {nome} \nPreço: R$ {preco:.2f}')
+                print('Parabéns! Seu produto foi cadastrado com sucesso!')
                 lista_cadastrados.append(produto[:])
-                novo = str(input('Cadastrar novo produto? [S/N]'))
+                novo = str(input('Cadastrar novo produto? [S/N] '))
                 while novo not in "SsNn":
                     print('Código inválido!')
                     novo = str(input('Digite [S] p/ sim ou [N] p/ não: '))
@@ -31,48 +31,61 @@ def MenuCadastro():
                 if novo in 'Nn':
                     break
         elif num == 2:
-            print('Lista de produtos cadastrados: ')
-            cont_01 = 0
-            print('=+' * 30)
-            print(f'{"Código":<7}{"Nome do Produto":<20}{"Preço":>10}')
-            print('=+' * 30)
-            while cont_01 < len(lista_cadastrados):
-                print(f'{cont_01:^7}{lista_cadastrados[cont_01][0]:.<20}R${lista_cadastrados[cont_01][1]:.>8.2f}')
-                cont_01 += 1
-        elif num == 3:
             while True:
-                print('[ÁREA DE DELEÇÃO] Lista de produtos cadastrados [ÁREA DE DELEÇÃO] ')
+                Tela.LimpaTela()
+                print('Lista de produtos cadastrados: ')
                 cont_01 = 0
-                print('=+' * 30)
+                print('=+' * 19)
                 print(f'{"Código":<7}{"Nome do Produto":<20}{"Preço":>10}')
-                print('=+' * 30)
+                print('=+' * 19)
                 while cont_01 < len(lista_cadastrados):
                     print(f'{cont_01:^7}{lista_cadastrados[cont_01][0]:.<20}R${lista_cadastrados[cont_01][1]:.>8.2f}')
                     cont_01 += 1
-                deletar = str(input('Quer deletar algum produto? [S/N]')).strip()[0]
+                pergunta = str(input('Mostrar lista novamente? [S/N] '))
+                while novo not in "SsNn":
+                    print('Código inválido!')
+                    pergunta = str(input('Digite [S] p/ sim ou [N] p/ não: '))
+                if pergunta in 'Ss':
+                    print()
+                elif pergunta in 'Nn':
+                    break
+        elif num == 3:
+            while True:
+                Tela.LimpaTela()
+                print('[CUIDADO! ÁREA DE DELEÇÃO] Lista de produtos cadastrados [ATENÇÃO! ÁREA DE DELEÇÃO] ')
+                cont_01 = 0
+                print('=+' * 19)
+                print(f'{"Código":<7}{"Nome do Produto":<20}{"Preço":>10}')
+                print('=+' * 19)
+                while cont_01 < len(lista_cadastrados):
+                    print(f'{cont_01:^7}{lista_cadastrados[cont_01][0]:.<20}R${lista_cadastrados[cont_01][1]:.>8.2f}')
+                    cont_01 += 1
+                deletar = str(input('Quer deletar algum produto? [S/N] ')).strip()[0]
                 while deletar not in "SsNn":
                     print('Código inválido!')
                     deletar = str(input('Digite [S] p/ sim ou [N] p/ não: '))
-                while True:
-                    if deletar in "Ss:":
-                        while True:
-                            num_del = int(input('Digite o código do produto a ser deletado: '))
-                            if 0 <= num_del <= len(lista_cadastrados) - 1:
-                                del lista_cadastrados[num_del]
-                                print('Produto deletado com sucesso!')
-                                break
-                            else:
-                                print('Código de produto inválido!')
-                        print('O que deseja fazer agora?')
-                        print('1. Deletar outro produto.')
-                        print('2. Voltar.')
-                        num = int(input('Digite a opção: '))
-                        if num == 1:
-                            print()
-                        elif num == 2:
+                if deletar in "Ss:":
+                    num_del = int(input('Digite o código do produto a ser deletado: '))
+                    while True:
+                        if 0 <= num_del <= len(lista_cadastrados) - 1:
+                            print(f'O Produto {lista_cadastrados[num_del][0]} foi deletado com sucesso!')
+                            del lista_cadastrados[num_del]
                             break
-                    else:
-                        break
+                        else:
+                            print('Código de produto inválido!')
+                            num_del = int(input('Digite o código do produto a ser deletado: '))
+                else:
+                    break
+                print('O que deseja fazer agora?')
+                print('1. Deletar outro produto.')
+                print('2. Voltar.')
+                num = int(input('Digite a opção: '))
+                if num == 1:
+                    print()
+                elif num == 2:
+                    break
+                else:
+                    break
         elif num == 4:
             break
-MenuCadastro()
+#MenuCadastro()
