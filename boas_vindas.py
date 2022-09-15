7ef95dfdf436dc339a651cb7da8569e0aefa00ca
clarabs <maria.meier@luizalabs.com>
2022-09-14 22:05:57 -0300

    correções bug

--- boas_vindas.py
+++ boas_vindas.py
@@ -4,13 +4,13 @@ executar = True
 while executar: 
     Tela.LimpaTela()
     print('Seja bem vindo ao Organico’s 🙂.\nAqui você pode comprar nossos produtos direto pelo seu celular!')
-    red = int(input('Digite o número de uma das opções abaixo: \n 1 - Menu de Navegação \n 2 - Encerrar o programa. \n'))
-    while red not in range(1, 3):
-        print('Por favor, digite o valor 1 ou 2.')
-        red = int(input())
-    if red == 1:
+    red = str(input('Digite o número de uma das opções abaixo: \n 1 - Menu de Navegação \n 2 - Encerrar o programa. \n Digite o que deseja: '))
+    while red not in '12':
+        print('Por favor, digite o valor 1 ou 2. \n Digite o que deseja: ')
+        red = str(input())
+    if red == '1':
         print('Que bom que está continuando com a gente! Você está sendo redirecionada (o) para o nosso menu de navegação!')
         menu_nav.nav()
-    elif red == 2:
+    elif red == '2':
         executar = False
         print('Obrigada por visitar nosso programa. Volte sempre!')
--- menu_nav.py
+++ menu_nav.py
@@ -8,14 +8,14 @@ def nav():
         Tela.LimpaTela()
         print('Que bom que está continuando com a gente! Você está sendo redirecionada (o) para o nosso menu de navegação!')
         print()
-        red2 = int(input('Digite o número de uma das opções abaixo: \n 1 - Menu de Cadastro; \n 2 - Menu de Vendas; \n 3 - Voltar.\n'))
-        while red2 not in range(1, 4):
-            print('Por favor, digite o valor 1, 2 ou 3.')
-            red2 = int(input())
-        if red2 == 1:
+        red2 = str(input('Digite o número de uma das opções abaixo: \n 1 - Menu de Cadastro; \n 2 - Menu de Vendas; \n 3 - Voltar. \n Digite o que deseja: '))
+        while red2 not in '123':
+            print('Por favor, digite o valor 1, 2 ou 3. \n Digite o que deseja: ')
+            red2 = str(input())
+        if red2 == '1':
             menu_cadastro.MenuCadastro()
-        elif red2 == 2:
+        elif red2 == '2':
             menu_vendas.menu_vendas()
-        elif red2 == 3:
+        elif red2 == '3':
            executar = False
         
